82bcda1f2d74883b6266e647024018df740af0e0
Raghu Saxena <poiasdpoiasd@live.com>
2023-07-15 14:41:58 +0800

    Allow bot_token, voice_channel_id overrides in CLI args

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::sync::Mutex;
 
 use log::{warn, error, debug, info};
 use songbird::{SerenityInit, input::{Input, Reader, Codec, reader::MediaSource, Container}};
+use clap::Parser;
 
 use std::env;
 
@@ -71,9 +72,17 @@ struct StdinmanConfig {
     voice_channel_id: String,
 }
 
+#[derive(Parser)]
+struct StdinmanArgs {
+    bot_token: Option<String>,
+    voice_channel_id: Option<String>
+}
+
 #[tokio::main]
 async fn main() { 
     env_logger::init();
+
+    let cfg_path = confy::get_configuration_file_path("stdinman", "stdinman").expect("fail to get config file path");
     let cfg: StdinmanConfig = match confy::load("stdinman", "stdinman"){
         Ok(c) => c,
         Err(e) => {
@@ -82,12 +91,32 @@ async fn main() {
         }
     };
 
-    // TODO: Config can be overridden via CLI params (i.e. value = CLI Param || Config)
-    if cfg.bot_token == "" || cfg.voice_channel_id == "" {
-        let cfg_path = confy::get_configuration_file_path("stdinman", "stdinman").expect("fail to get config file path");
-        error!("missing bot_token / voice_channel_id in config! Please add it in {}", cfg_path.to_string_lossy());
-        std::process::exit(-1);
-    }
+    // First try CLI, then config on disk
+    // (to allow overrides on via CLI)
+    let args = StdinmanArgs::parse();
+    let bot_token = match args.bot_token {
+        Some(token) => token,
+        None => {
+            if cfg.bot_token == "" {
+                error!("missing bot_token in config! Please provide it via CLI arg or add it in {}", cfg_path.to_string_lossy());
+                std::process::exit(-1);
+            }
+
+            cfg.bot_token
+        }
+    };
+
+    let voice_channel_id = match args.voice_channel_id {
+        Some(token) => token,
+        None => {
+            if cfg.voice_channel_id == "" {
+                error!("missing voice_channel_id in config! Please provide it via CLI arg or add it in {}", cfg_path.to_string_lossy());
+                std::process::exit(-1);
+            }
+
+            cfg.voice_channel_id
+        }
+    };
 
     let (tx, rx) = mpsc::channel::<bool>();
 
@@ -112,10 +141,10 @@ async fn main() {
 
     let framework = StandardFramework::new();
     let intents = GatewayIntents::non_privileged();
-    let mut client = Client::builder(cfg.bot_token, intents)
+    let mut client = Client::builder(bot_token, intents)
         .register_songbird()
         .event_handler(Handler{ 
-            voice_channel_id: cfg.voice_channel_id,
+            voice_channel_id: voice_channel_id,
             tx: tx.into(),
         })
         .framework(framework)
